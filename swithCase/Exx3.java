c789548355a28bddfa8ba9c746bfa85b8fd006cb
isah17 <119014328+isah17@users.noreply.github.com>
2022-12-02 10:16:44 -0300

    Add files via upload

---
+++ swithCase/Exx3.java
@@ -0,0 +1,33 @@
+package swithCase;
+import java.util.Scanner;
+public class Exx3 {
+
+	public static void main(String[] args) {
+	Scanner ler = new Scanner (System.in);
+			
+			int i;
+			
+			System.out.println("Digite a idade do aluno");
+			i = ler.nextInt();
+			switch (i) {
+			case 6:
+				System.out.println("Dente de leite");
+				break;
+			case 7:
+				System.out.println("junior");
+				break;
+			case 8:
+				System.out.println("junior max");
+				break;
+			case 9:
+				System.out.println("junior master");
+				break;
+			case 10:
+				System.out.println("master");
+				break;
+				default:
+					System.out.println("A escola não admite alunos nessa idade");
+			}
+	}
+
+}
