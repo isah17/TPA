55e88d4460c88a60b3f4be57013a44b82df94dd6
isah17 <119014328+isah17@users.noreply.github.com>
2022-12-02 10:17:53 -0300

    Add files via upload

---
+++ vetor/Exercicio1.java
@@ -0,0 +1,28 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		
+		final int t = 10;
+		int i;
+		double  b[];
+		int a[];
+		
+		a = new int [t];
+		b = new double [t];
+		
+		for(i=0; i<t; i++) {
+			System.out.println("Entre com o " +(i+1)+ "º valor");
+			a[i] = ler.nextInt();
+			b[i]= Math.sqrt(a[i]);
+			System.out.println(b[i]);
+			
+			
+		}
+	}
+
+}
